cf9df885b021586e9d400da58b5f1d323c7a4a53
Prince_Corn <konradlippertzajaczkowski@gmail.com>
2017-04-24 10:43:11 -0500

    FIX: added a null check for incompatible circumstances

--- app/instance-initializers/browser/cache-rendered.js
+++ app/instance-initializers/browser/cache-rendered.js
@@ -2,9 +2,11 @@ import Ember from 'ember';
 import config from '../../config/environment';
 
 export function initialize( appInstance ) {
-  navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
-    serviceWorkerRegistration.active.postMessage([location.protocol, '//', location.host, location.pathname].join(''));
-  });
+  if (navigator.serviceWorker && navigator.serviceWorker.ready) {
+    navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
+      serviceWorkerRegistration.active.postMessage([location.protocol, '//', location.host, location.pathname].join(''));
+    });
+  }
 }
 
 export default {
