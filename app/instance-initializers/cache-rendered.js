5adfb858e541caba779608cb2982c8064034cdb2
Prince_Corn <konradlippertzajaczkowski@gmail.com>
2017-05-31 22:31:21 -0500

    removed unneeded references

--- app/instance-initializers/cache-rendered.js
+++ app/instance-initializers/cache-rendered.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
-import config from '../config/environment';
 
-export function initialize( appInstance ) {
+export function initialize() {
   if (typeof FastBoot === 'undefined' && navigator.serviceWorker && navigator.serviceWorker.ready) {
     navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
       serviceWorkerRegistration.active.postMessage([location.protocol, '//', location.host, location.pathname].join(''));
